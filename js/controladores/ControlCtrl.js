f6d4e2e520684dfea4265c9a2e784760c22f1cba
ProyectoVotaciones11 <jhancarlos5566@gmail.com>
2018-08-02 16:10:49 -0500

    se añadio un modal

--- js/controladores/ControlCtrl.js
+++ js/controladores/ControlCtrl.js
@@ -1,7 +1,7 @@
 angular.module('votacioneslive')
 
 
-.controller('ControlCtrl', function($scope, $state,  AuthServ, $q, toastr, $http, MySocket){
+.controller('ControlCtrl', function($scope, $state,  AuthServ, $q, toastr, $http, MySocket, $uibModal){
 
 	MySocket.on('me_recibieron_logueo', function(data){
 		
@@ -10,14 +10,53 @@ angular.module('votacioneslive')
 	});	  
 
 
-
 	MySocket.on('clientes_traidos', function(data){
 		
 		 console.log(data);
  
 		 $scope.puntos = data;
 
-	});	  
+	});	 
+
+	$scope.Ver_Usuario = function(punto){
 
 
-});
+	    var modalInstance = $uibModal.open({
+	        templateUrl: 'templates/Usuario_puntos.html',
+	        resolve: {
+		        punto: function () {
+		        	return  punto;
+		        }
+		    },
+	        controller: 'Usuario_punto' // En LibroMesModales.js 
+	    });
+
+	    modalInstance.result.then(function (result) {
+			console.log(result);
+	    }, function(r2){
+	    	$scope.traerDatos();
+	    });
+			
+	} 
+
+
+})
+
+.controller("Usuario_punto", function($uibModalInstance, $scope, punto, ConexionServ, toastr, $filter) {
+
+	
+
+    $scope.punto = punto;
+
+   console.log(punto);
+
+    $scope.ok = function () {
+        $uibModalInstance.close('Cerrado');
+    };
+
+
+    
+
+
+    return ;
+})

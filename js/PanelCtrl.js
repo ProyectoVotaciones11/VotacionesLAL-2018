d7ff48dca364c6c7e053a84aea97b5abd588582e
ProyectoVotaciones11 <jhancarlos5566@gmail.com>
2018-10-17 13:01:28 -0500

    cambio en usuarios

--- js/PanelCtrl.js
+++ js/PanelCtrl.js
@@ -4,6 +4,34 @@ angular.module('votacioneslive')
     
     $scope.USER = USER;
 
+
+    $scope.descargarDatos = function(){
+
+       $http.get('http://192.168.100.31/5myvc/public/asistencias/datos-solo-alumnos', { year_id: 4 }).then(function(result){
+            grupos = result.data;
+            console.log(grupos);
+
+            $http.get('::usuarios/Subir_Datos', { grupos: grupos }).then(function(result){
+            /*
+            for (var i = 0; i < grupos.length; i++) {
+                 console.log(grupos[i].alumnos);
+
+                 for (var j = 0; j < grupos[j].alumnos.length; j++) {
+                     alumno = grupos[i].alumnos[j];
+
+                     consulta = 'INSERT invdfvlsdjf (alumno['nombres'], alumno.apellidos, grupos[i].abrev)
+                 }
+            }
+            */
+            
+        }, function(){
+            console.log('Error descargando datos');
+        });
+
+        });
+
+    }
+
  
 
     console.log($scope.USER);
